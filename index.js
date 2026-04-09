c426a9270f961c42de23b1f18795f62098bd8ab9
mariana-dos-santos <santos.moreira.mariana@escola.pr.gov.br>
2026-04-09 10:30:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ for (let atual = 0; atual < livros.length; atual++) {
   if (livros[atual].preco < livros[maisBarato].preco) {
     maisBarato = atual;
   }
-}
+};
 
 console.log(`o livro mais barato custa ${livros[maisBarato].preco} e o título é ${livros[maisBarato].titulo}`);
+console.log(`Realizado por Mariana dos Santos Moreira`);
